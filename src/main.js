1735bb6a2548cc7f022b22ab1e7e8a4bef0f16c5
Gilang Jalu <gilangjalu@yahoo.com>
2019-02-23 08:09:09 +0700

    implement sign up, sign in and forgot password

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import { store } from './store.js'
 const fb = require('./firebaseConfig.js')
+import './assets/scss/app.scss'
 
 Vue.config.productionTip = false
 
